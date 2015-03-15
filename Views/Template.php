159c9409fea8f7653346da1e83d7f92e45061780
jriver27 <jrivera@uh.edu>
2015-03-14 21:01:35 -0500

    Updating Folder

---
+++ Views/Template.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head lang="en">
+    <title>Medical Inventory Login</title>
+    <style type="text/css">
+        body {
+            background-color: lightblue;
+        }
+
+        #header {
+            background-color: darkblue;
+        }
+
+        #footer {
+            text-align: left;
+            border-top: dashed;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        label {
+            width: 150px;
+            display: block;
+            float: left;
+            margin: auto;
+        }
+
+        input[type=button] {
+            background-color: darkblue;
+            font-family: serif;
+            color: azure;
+            width: 16%;
+            float: inherit;
+            margin: auto;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="header">
+        <h1>Medical Inventory Login</h1>
+    </div>
+<?php
+    echo "My first PHP script!";
+?>
+    <div id="put anything here">
+    </div>
+
+    <div id="footer">
+        <span> Please Contact Us anytime.</span>
+    </div>
+</body>
+
+</html>
