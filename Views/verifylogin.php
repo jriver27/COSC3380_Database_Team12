6632b2a6a7f6166e31ef8a8c7abe1eb6013dae78
alisalimabd <alisalimabd@gmail.com>
2015-04-05 11:38:30 -0500

    Update verifylogin.php
    succesfully grabs position name from the database

--- Views/verifylogin.php
+++ Views/verifylogin.php
@@ -4,7 +4,7 @@ include 'dbconnect.php';
 $username = trim($_POST['username']);
 $password = trim($_POST['password']);
 
-$sql="SELECT Username, Position  FROM users WHERE username='$username' and password='$password'";
+$sql="SELECT users.Username, user_position.Position FROM users JOIN user_position ON users.position = user_position.ID WHERE users.Username='$username' and users.password='$password'";
 $result=mysqli_query($link, $sql);
 $count=mysqli_num_rows($result);
 
